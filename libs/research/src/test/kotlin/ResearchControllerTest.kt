75d1c3db43b567e6510ca3a4188c3f40e37a6417
chadmarchand <37517821+chadmarchand@users.noreply.github.com>
2021-01-13 17:23:59 -0600

    convert to multi module gradle project (#17)

--- libs/research/src/test/kotlin/ResearchControllerTest.kt
+++ libs/research/src/test/kotlin/ResearchControllerTest.kt
@@ -1,13 +1,12 @@
 package com.chadmarchand.forgeofhistory.research
 
-import com.chadmarchand.forgeofhistory.ForgeOfHistoryTestExtension
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.koin.core.component.inject
 import org.koin.test.KoinTest
 
-@ExtendWith(ForgeOfHistoryTestExtension::class)
+@ExtendWith(ResearchTestExtension::class)
 class ResearchControllerTest : KoinTest {
     private val researchController: ResearchController by inject()
 
