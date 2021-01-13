4cf287bf84ca20fab4d1d16a89a35c515028c74f
chad <chadmarchand@gmail.com>
2021-01-13 17:09:59 -0600

    convert to multi module gradle project

--- libs/research/src/test/kotlin/ResearchTestExtension.kt
+++ libs/research/src/test/kotlin/ResearchTestExtension.kt
@@ -1,13 +1,12 @@
-package com.chadmarchand.forgeofhistory
+package com.chadmarchand.forgeofhistory.research
 
-import com.chadmarchand.forgeofhistory.research.researchModule
 import com.chadmarchand.kutna.event.EventBus
 import com.chadmarchand.kutna.persistence.KutnaTestSetupExtensionPersistent
 import com.chadmarchand.kutna.persistence.persistenceModule
 import org.koin.dsl.module
 import org.koin.experimental.builder.single
 
-class ForgeOfHistoryTestExtension : KutnaTestSetupExtensionPersistent(
+class ResearchTestExtension : KutnaTestSetupExtensionPersistent(
     listOf(
         persistenceModule,
         researchModule,
